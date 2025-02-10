df404f0112eda344a20359787135955204aaba15
Riktam Nandi <riktamrdxnandi@gmail.com>
2025-02-10 12:07:09 +0530

    minor changes

--- src/pages/SocialMediaIcon.jsx
+++ src/pages/SocialMediaIcon.jsx
@@ -7,13 +7,17 @@ function SocialMediaIcon() {
   return (
     <div className="lg:flex hidden flex-col bg-white shadow-xs px-2 py-3 rounded-sm gap-2 mt-20 fixed left-4 top-56 z-1000">
       <a
-        href="#"
+        href="https://www.linkedin.com/in/riktam-nandi-1a0449319/"
+        target="_blank"
+        rel="noopener noreferrer"
         className="flex items-center justify-center border-2 border-orange-300 rounded-full w-[35px] h-[35px]"
       >
         <FaLinkedin className="text-orange-300 text-xl" />
       </a>
       <a
-        href="#"
+        href="https://github.com/rdxnandi"
+        target="_blank"
+        rel="noopener noreferrer"
         className="flex items-center justify-center border-2 border-orange-300 rounded-full w-[35px] h-[35px]"
       >
         <FaGithub className="text-orange-300 text-xl" />
@@ -25,7 +29,9 @@ function SocialMediaIcon() {
         <FaSquareXTwitter className="text-orange-300 text-xl" />
       </a>
       <a
-        href="#"
+        href="https://www.frontendmentor.io/profil/rdxnandi"
+        target="_blank"
+        rel="noopener noreferrer"
         className="flex items-center justify-center border-2 border-orange-300 rounded-full w-[35px] h-[35px]"
       >
         <SiFrontendmentor className="text-orange-300 text-xl" />
